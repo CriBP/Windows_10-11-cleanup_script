6583042f87ec5c79bf451a36e51f60c578e0a162
Cris P <105011867+CriBP@users.noreply.github.com>
2024-12-05 18:17:16 -0800

    Update WinClean.ps1
    minor updates

--- WinClean.ps1
+++ WinClean.ps1
@@ -15,8 +15,8 @@ Start-Sleep 1
 Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
 Exit
 }
-
-$DebloatFolder = "C:\Temp\Windows10Debloater"
+$docdir = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" -Name Personal
+$DebloatFolder = $docdir+"\PC-info"
 If (Test-Path $DebloatFolder) {
 Write-Output "$DebloatFolder exists. Skipping."
 }
@@ -38,8 +38,8 @@ Write-Verbose -Message ('Starting Sysprep Fixes')
 New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
 
 # view a list of installed apps:
-Get-AppxPackage -AllUsers | Select Name, PackageFullName
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize
+Get-AppxPackage -AllUsers | Select Name, PackageFullName | Out-File -filepath "$DebloatFolder\installed-apps-before-cleaning.txt"
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize | Out-File -filepath "$DebloatFolder\installed-programs-before-cleaning.txt"
 
 Write-Output "Removing bloatware apps."
 [regex]$WhitelistedApps ='Microsoft.VP9VideoExtensions|Microsoft.NET.Native|Microsoft.HEVCVideoExtension|Microsoft.MicrosoftStickyNotes|Microsoft.Paint|Microsoft.RawImageExtension|Microsoft.WindowsNotepad|Microsoft.WindowsSoundRecorder|Microsoft.LanguageExperiencePackro-RO|Microsoft.AAD.BrokerPlugin|Microsoft.AccountsControl|Microsoft.AsyncTextService|Microsoft.BioEnrollment|Microsoft.CredDialogHost|Microsoft.ECApp|Microsoft.LockApp|Microsoft.UI.Xaml.CBS|Microsoft.Win32WebViewHost|Microsoft.Windows.Apprep.ChxApp|Microsoft.Windows.CallingShellApp|Microsoft.Windows.CapturePicker|Microsoft.Windows.CloudExperienceHost|Microsoft.Windows.ContentDeliveryManager|Microsoft.Windows.FileExplorer|Microsoft.Windows.NarratorQuickStart|Microsoft.Windows.OOBENetworkCaptivePortal|Microsoft.Windows.OOBENetworkConnectionFlow|Microsoft.Windows.ParentalControls|Microsoft.Windows.PeopleExperienceHost|Microsoft.Windows.PinningConfirmationDialog|Microsoft.Windows.Search|Microsoft.Windows.ShellExperienceHost|Microsoft.Windows.XGpuEjectDialog|MicrosoftWindows.UndockedDevKit|NcsiUwpApp|windows.immersivecontrolpanel|Windows.PrintDialog|Microsoft.UI.Xaml|Microsoft.VCLibs|Microsoft.Windows.StartMenuExperienceHost|Microsoft.WebMediaExtensions|Microsoft.WebpImageExtension|Microsoft.WindowsCalculator|Microsoft.DesktopAppInstaller|Microsoft.WindowsTerminal|Microsoft.HEIFImageExtension|Microsoft.Windows.Photos|Microsoft.WindowsStore|Microsoft.ScreenSketch|Microsoft.WindowsCamera|Microsoft.Winget.Source'
@@ -52,10 +52,10 @@ Get-AppxPackage | Where-Object {$_.Name -NotMatch $WhitelistedApps} | Remove-App
 }
 
 # To restore all Apps use: Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
-Write-Output "All Apps remaining:..."
-Get-AppxPackage -AllUsers | Select Name, PackageFullName
+Write-Output "All Apps remaining after cleanup:..."
+Get-AppxPackage -AllUsers | Select Name, PackageFullName | Out-File -filepath "$DebloatFolder\installed-apps-after-cleaning.txt"
 
-#Cleanup Start Menu
+# Cleanup Start Menu
 Write-Output "Cleaning up the Start Menu Apps"
 # https://superuser.com/a/1442733 #Requires -RunAsAdministrator
 
@@ -97,20 +97,20 @@ $START_MENU_LAYOUT = @"
 </LayoutModificationTemplate>
 "@
 
-$layoutFile="C:\Windows\StartMenuLayout.xml"
+$layoutFile="$DebloatFolder\StartMenuLayout.xml"
 
-#Delete layout file if it already exists
+# Delete layout file if it already exists
 If(Test-Path $layoutFile)
 {
     Remove-Item $layoutFile
 }
 
-#Creates the blank layout file
+# Creates the blank layout file
 $START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
 
 $regAliases = @("HKLM", "HKCU")
 
-#Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
+# Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
 foreach ($regAlias in $regAliases){
     $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
     $keyPath = $basePath + "\Explorer" 
@@ -121,28 +121,28 @@ foreach ($regAlias in $regAliases){
     Set-ItemProperty -Path $keyPath -Name "StartLayoutFile" -Value $layoutFile
 }
 
-#Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
+# Restart Explorer, open the start menu (necessary to load the new layout), and give it a few seconds to process
 Stop-Process -name explorer
 Start-Sleep -s 5
 $wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^{ESCAPE}')
 Start-Sleep -s 5
 
-#Enable the ability to pin items again by disabling "LockedStartLayout"
+# Enable the ability to pin items again by disabling "LockedStartLayout"
 foreach ($regAlias in $regAliases){
     $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
     $keyPath = $basePath + "\Explorer" 
     Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 0
 }
 
-#Restart Explorer and delete the layout file
+# Restart Explorer and delete the layout file
 Stop-Process -name explorer
 
 # Uncomment the next line to make clean start menu default for all new users
 #Import-StartLayout -LayoutPath $layoutFile -MountPath $env:SystemDrive\
 
-Remove-Item $layoutFile
+# Remove-Item $layoutFile
 
-#Disable Memory Compression
+# Disable Memory Compression
 Get-MMAgent
 Disable-MMAgent -alp
 Disable-MMAgent -apl
